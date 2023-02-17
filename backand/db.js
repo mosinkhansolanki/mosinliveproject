665c7da16177e328b5d1fe27f877fbac2273da9f
Name mosinkhansolanki <mosinkhansolankiy392@gmail.com>
2023-02-17 12:47:58 +0530

    second commit

--- backand/db.js
+++ backand/db.js
@@ -4,7 +4,7 @@ const connectdb = async () => {
     try {
         const condb = await mongoose.connect(process.env. MONGO_URL);
         console.log("hello this result==>",connectdb);
-         console.log("hello world");
+         console.log("hellooooooo");
     }
     catch (err) {
         console.log(err);
